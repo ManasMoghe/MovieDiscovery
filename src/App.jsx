6557557590a2c0e2098b54c0fe29b3643d269abb
Manas Moghe <mxlieutenant@gmail.com>
2026-02-05 23:42:00 +0530

    Initial commit - MovieDiscovery

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const searchMovies = async () => {
+    if (!query) return;
+
+    setLoading(true);
+
+    const res = await fetch(
+      `https://www.omdbapi.com/?apikey=d75d3373&s=${query}`
+    );
+
+    const data = await res.json();
+    setMovies(data.Search || []);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    const delayDebounce = setTimeout(() => {
+      if (!query) {
+        setMovies([]);
+        return;
+      }
+      searchMovies();
+
+    }, 500);
+
+    return () => clearTimeout(delayDebounce);
+  }, [query]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ padding: "30px", fontFamily: "Arial" }}>
+      <h1>Movie Discovery</h1>
+
+      <input
+        value={query}
+        placeholder="Search movies..."
+        onChange={(e) => setQuery(e.target.value)}
+        style={{
+          padding: "8px",
+          width: "250px",
+        }}
+      />
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && movies.length === 0 && query && (
+        <p>No movies found.</p>
+      )}
+
+
+      <div
+        style={{
+          marginTop: "30px",
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, 200px)",
+          gap: "20px",
+        }}
+      >
+        {movies.map((movie) => (
+          <div key={movie.imdbID}>
+            <img
+              src={
+                movie.Poster && movie.Poster !== "N/A"
+                  ? movie.Poster
+                  : "https://via.placeholder.com/200x300?text=No+Image"
+              }
+              width="200"
+            />
+            <h3>{movie.Title}</h3>
+            <p>{movie.Year}</p>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
